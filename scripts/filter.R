cfc7872e6a09332702deaa4217e3e391f31aa7b3
Allison DeLoache <allison.deloache@selu.edu>
2022-09-27 20:20:47 +0000

    Revert "summer temp selection"
    This reverts commit 0a50514467d519bc17637b1f71bb07a202608548.

--- scripts/filter.R
+++ scripts/filter.R
@@ -7,9 +7,3 @@ filter_years <- function(dataset = data){
     
   return(new_data)
 }
-
-summer_temp_selection <- function(dataset = data){
-  sum_temp <- data %>% 
-    filter(SummerTemp > 15.0)
-  return(sum_temp)
-}
